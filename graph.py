d2589b2086245cde7e47ebe7989606184ee3d6c0
Keziah Plattner <keziah@cs.stanford.edu>
2016-05-24 05:23:54 +0000

    graphing stuff

--- graph.py
+++ graph.py
@@ -1,23 +1,14 @@
 import sys
 from optparse import OptionParser
+import matplotlib.pyplot as plt
 
-# Reads in a file as a map of percentage_of_sample => {fp: x, tp: y}
-# TODO
-def read_file(filename):
-    f = open(filename, 'r')
+# y is either the TP rate or the FP rate
+# sampling_type: string that states the sampling type, used for label    
+def graph(sampling_rate, y, sampling_type):
+    plt.plot(sampling_rate, y)
+    plt.xlabel('Sampling Rate')
+    plt.ylabel(sampling_type)
+    plt.show()
 
-def main(argv, options):
-    mp = read_file(options.filename)
 
-if __name__ == "__main__": 
-    parser = OptionParser()
-    parser.add_option("-s", "--systematic", action="store_true", \
-        help="Systematic Sampling", dest="systematic")
-    parser.add_option("-r", "--random", action="store_true", \
-        help="Random 1 in N Sampling", dest="random")
-    parser.add_option("-u", "--uniform", action="store_true", \
-        help="Random 1 in N Sampling", dest="uniform")
-    parser.add_option("-f", "--file", \
-        action="store", type="string", dest="filename")
-    options, args = parser.parse_args()
-    main(args[1:], options)
+graph([1, 2, 3], [2, 3, 4], "test")
